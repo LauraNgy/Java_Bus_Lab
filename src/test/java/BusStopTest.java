080b56392ba32201eb914600ef0266bd7a1f4518
chloe alcorn <ca.alcorn@outlook.com>
2018-10-09 13:23:51 +0100

    app passenger to Q

---
+++ src/test/java/BusStopTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusStopTest {
+
+    BusStop busStop;
+    Person person;
+
+    @Before
+    public void before(){
+        busStop = new BusStop("Princess Street");
+        person = new Person();
+    }
+
+    @Test
+    public void addPersonToQueue(){
+        busStop.addPersonToQueue(person);
+        assertEquals(1, busStop.queueCount());
+    }
+
+}
